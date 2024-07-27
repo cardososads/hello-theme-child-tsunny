de8d492fc31fd06bad709906f3a4a5593e1ffd8e
Wesley Cardoso <cardososads@github.com>
2024-07-27 17:26:15 -0400

    show_session_data

--- functions.php
+++ functions.php
@@ -33,16 +33,6 @@ function hello_elementor_child_scripts_styles() {
 	);
 
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
-
-function redirect_non_logged_users_to_login()
-{
-	if (!is_user_logged_in() && !is_page('login')) { // Verifica se o usuário não está logado e não está na página de login
-		wp_redirect(home_url('/login')); // Redireciona para a página de login
-		exit();
-	}
-}
-add_action('template_redirect', 'redirect_non_logged_users_to_login');
 
 function redirect_non_logged_users_to_login()
 {
