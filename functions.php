779e72315e0096d49c79248d49f8d415944d7b7b
Wesley Cardoso <cardososads@github.com>
2024-07-29 00:30:19 -0400

    redirect_non_logged_users_to_login

--- functions.php
+++ functions.php
@@ -34,11 +34,11 @@ function hello_elementor_child_scripts_styles() {
 
 }
 
-function redirect_non_logged_users_to_login()
+function redirect_non_logged_users()
 {
-	if (!is_user_logged_in() && !is_page('login')) {
+	if (!is_user_logged_in()) {
 		wp_redirect(site_url('/login'));
 		exit;
 	}
 }
-add_action('template_redirect', 'redirect_non_logged_users_to_login');
+add_action('template_redirect', 'redirect_non_logged_users');
